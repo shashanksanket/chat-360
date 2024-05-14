1803ca4355b19f943d30b87d8b4867df247f9c34
shashanksanketdev <shashank.sanket014@gmail.com>
2024-05-14 21:35:13 +0530

    fix

--- backend/dist/index.js
+++ backend/dist/index.js
@@ -15,7 +15,9 @@ app.get('/', (req, res) => {
     res.send("Welcome");
 });
 app.use((0, cors_1.default)());
-app.get('/api/v1/logs', (req, res) => {
+route_1.default.get('/api/v1/logs', (req, res) => {
+    const page = parseInt(req.query.page, 10) || 1;
+    const limit = parseInt(req.query.limit, 10) || 1;
     const logFolder = path_1.default.join(__dirname, '..', 'logs');
     const logFiles = fs_1.default.readdirSync(logFolder);
     const logs = [];
@@ -32,7 +34,15 @@ app.get('/api/v1/logs', (req, res) => {
         });
         logs.push(...parsedLogs.filter((log) => log));
     });
-    res.json(logs);
+    const startIndex = (page - 1) * limit;
+    const endIndex = page * limit;
+    const paginatedLogs = logs.slice(startIndex, endIndex);
+    res.json({
+        logs: paginatedLogs,
+        currentPage: page,
+        totalPages: Math.ceil(logs.length / limit),
+        totalLogs: logs.length
+    });
 });
 app.listen(port, () => {
     console.log(`Server is running on http://localhost:${port}`);
